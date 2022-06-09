da48598e7265a1c8d6bd96dc75e739b962218255
Angel Felix <105471577+Afelix997@users.noreply.github.com>
2022-06-09 15:05:44 -0700

    Update binary_search.py

--- python/binary_search.py
+++ python/binary_search.py
@@ -1,2 +1,19 @@
-def binary_search():
-    pass
+
+def binary_search(num, val_arr):
+    min = val_arr[0]
+    max = val_arr[len(val_arr)-1]
+
+    mid = (max+min)//2
+    print(num, mid)
+
+    if num != mid and len(val_arr) <= 1:
+        return -1
+
+    if num == mid:
+        return True
+    elif num > mid:
+        val_arr = val_arr[len(val_arr)//2:]
+        return binary_search(num,val_arr)
+    elif num < mid:
+        val_arr = val_arr[:len(val_arr)//2]
+        return binary_search(num,val_arr)
